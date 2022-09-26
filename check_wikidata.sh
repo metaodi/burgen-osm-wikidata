ab23031fcbceb30e092a54219482cc19d05d7850
Stefan Oderbolz <oderbolz@gmail.com>
2022-09-26 23:54:22 +0200

    Rename query files

--- check_wikidata.sh
+++ check_wikidata.sh
@@ -5,4 +5,4 @@ set -o pipefail
 
 DIR="$(cd "$(dirname "$0")" && pwd)"
 
-cat $DIR/queries/overpass_castle_query.txt | $DIR/lib/overpass.py | $DIR/lib/parse_osm_data.py | $DIR/lib/generate_wd_query.py | $DIR/lib/parse_wikidata_query.py
+cat $DIR/queries/overpass_castle_query.overpassql | $DIR/lib/overpass.py | $DIR/lib/parse_osm_data.py | $DIR/lib/generate_wd_query.py | $DIR/lib/parse_wikidata_query.py
