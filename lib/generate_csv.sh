b89c6b1349a3a8efd0119340d2dddba77344a4ae
Stefan Oderbolz <oderbolz@gmail.com>
2022-09-26 23:53:27 +0200

    Rename query files

--- lib/generate_csv.sh
+++ lib/generate_csv.sh
@@ -5,4 +5,4 @@ set -o pipefail
 DIR="$(cd "$(dirname "$0")" && pwd)"
 
 echo "name,type,id,lat,lon,historic,site_type"
-cat $DIR/../queries/overpass_castle_query.txt | $DIR/overpass.py | $DIR/parse_osm_data.py
+cat $DIR/../queries/overpass_castle_query.overpassql | $DIR/overpass.py | $DIR/parse_osm_data.py
